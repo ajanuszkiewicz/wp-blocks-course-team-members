3393b503c5ad62cc4b3a2b41255e3c39854f6cdd
Adam Januszkiewicz <ajanuszkiewicz@gmail.com>
2023-05-25 13:37:36 -0400

    Add new Edit funciton for inner block

--- src/team-member/index.js
+++ src/team-member/index.js
@@ -1,11 +1,12 @@
 import { registerBlockType } from '@wordpress/blocks';
 import { __ } from '@wordpress/i18n';
+import Edit from './edit';
 
 registerBlockType('blocks-course/team-member', {
 	title: __('Team Member', 'team-members'),
 	description: __('A team member item', 'team-members'),
 	icon: 'admin-users',
 	parent: ['blocks-course/team-members'],
-	edit: () => <p>edit</p>,
+	edit: Edit,
 	save: () => <p>save</p>,
 });
