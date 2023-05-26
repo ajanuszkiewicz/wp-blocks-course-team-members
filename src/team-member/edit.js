48a1a6dfb8f000f4e9893f60f1bb9cecb1e7f547
Adam Januszkiewicz <ajanuszkiewicz@gmail.com>
2023-05-26 08:34:31 -0400

    Disable buttons to add images if an image has already been added

--- src/team-member/edit.js
+++ src/team-member/edit.js
@@ -4,25 +4,51 @@ import {
 	MediaPlaceholder,
 } from '@wordpress/block-editor';
 import { __ } from '@wordpress/i18n';
+import { isBlobURL } from '@wordpress/blob';
+import { Spinner } from '@wordpress/components';
 
 export default function Edit({ attributes, setAttributes }) {
-	const { name, bio } = attributes;
+	const { name, bio, url, alt } = attributes;
 	const onChangeName = (newName) => {
 		setAttributes({ name: newName });
 	};
 	const onChangeBio = (newBio) => {
 		setAttributes({ bio: newBio });
 	};
+	const onSelectImage = (image) => {
+		if (!image || !image.url) {
+			setAttributes({ url: undefined, id: undefined, alt: '' });
+			return;
+		}
+		setAttributes({ url: image.url, id: image.id, alt: image.alt });
+	};
+	const onSelectURL = (newURL) => {
+		setAttributes({
+			url: newURL,
+			id: undefined,
+			alt: '',
+		});
+	};
 	return (
 		<div {...useBlockProps()}>
+			{url && (
+				<div
+					className={`wp-block-blocks-course-team-member-img${
+						isBlobURL(url) ? ' is-loading' : ''
+					}`}
+				>
+					<img src={url} alt={alt} />
+					{isBlobURL(url) && <Spinner />}
+				</div>
+			)}
+
 			<MediaPlaceholder
 				icon="admin-users"
-				// eslint-disable-next-line no-console
-				onSelect={(val) => console.log(val)}
-				// eslint-disable-next-line no-console
-				onSelectURL={(val) => console.log(val)}
+				onSelect={onSelectImage}
+				onSelectURL={onSelectURL}
 				accept="image/*"
 				allowedTypes={['image']}
+				disableMediaButtons={url}
 			/>
 			<RichText
 				placeholder={__('Member Name', 'team-member')}
