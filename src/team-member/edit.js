37458b3ed1c5ddb3b80a14d35ba1821a0b127512
Adam Januszkiewicz <ajanuszkiewicz@gmail.com>
2023-05-27 19:32:06 -0400

    Add and style a box to edit the social media icons and links

--- src/team-member/edit.js
+++ src/team-member/edit.js
@@ -21,6 +21,8 @@ import {
 	SelectControl,
 	Icon,
 	Tooltip,
+	TextControl,
+	Button,
 } from '@wordpress/components';
 
 function Edit({
@@ -271,6 +273,16 @@ function Edit({
 						)}
 					</ul>
 				</div>
+				{selectedLink !== undefined && (
+					<div className="wp-block-blocks-course-team-member-link-form">
+						<TextControl label={__('Icon', 'team-members')} />
+						<TextControl label={__('URL', 'team-members')} />
+						<br />
+						<Button isDestructive>
+							{__('Remove Link', 'team-members')}
+						</Button>
+					</div>
+				)}
 			</div>
 		</>
 	);
