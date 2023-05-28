bbde3c6e48072736ae9bde3059dda25670903c99
Adam Januszkiewicz <ajanuszkiewicz@gmail.com>
2023-05-28 00:01:38 -0400

    Use @dnd-kit component library to add the ability to drag and drop social media icons horizontally to update their position

--- src/team-member/edit.js
+++ src/team-member/edit.js
@@ -8,6 +8,19 @@ import {
 	InspectorControls,
 	store as blockEditorStore,
 } from '@wordpress/block-editor';
+import {
+	DndContext,
+	useSensor,
+	useSensors,
+	PointerSensor,
+} from '@dnd-kit/core';
+import {
+	SortableContext,
+	horizontalListSortingStrategy,
+	arrayMove,
+} from '@dnd-kit/sortable';
+import { restrictToHorizontalAxis } from '@dnd-kit/modifiers';
+import SortableItem from './sortable-item';
 import { __ } from '@wordpress/i18n';
 import { useSelect } from '@wordpress/data';
 import { usePrevious } from '@wordpress/compose';
@@ -38,6 +51,14 @@ function Edit({
 
 	const prevURL = usePrevious(url);
 	const prevIsSelected = usePrevious(isSelected);
+
+	const sensors = useSensors(
+		useSensor(PointerSensor, {
+			activationConstraint: {
+				distance: 5,
+			},
+		})
+	);
 
 	const titleRef = useRef();
 
@@ -124,6 +145,23 @@ function Edit({
 			],
 		});
 		setSelectedLink();
+	};
+
+	const handleDragEnd = (event) => {
+		const { active, over } = event;
+		if (active && over && active.id !== over.id) {
+			const oldIndex = socialLinks.findIndex(
+				(i) => active.id === `${i.icon}-${i.link}`
+			);
+			const newIndex = socialLinks.findIndex(
+				(i) => over.id === `${i.icon}-${i.link}`
+			);
+
+			setAttributes({
+				socialLinks: arrayMove(socialLinks, oldIndex, newIndex),
+			});
+			setSelectedLink(newIndex);
+		}
 	};
 
 	const onChangeAlt = (newAlt) => {
@@ -248,28 +286,31 @@ function Edit({
 				/>
 				<div className="wp-block-blocks-course-team-member-social-links">
 					<ul>
-						{socialLinks.map((item, index) => {
-							return (
-								<li
-									key={index}
-									className={
-										selectedLink === index
-											? 'is-selected'
-											: null
-									}
-								>
-									<button
-										aria-label={__(
-											'Edit social link for team member',
-											'team-members'
-										)}
-										onClick={() => setSelectedLink(index)}
-									>
-										<Icon icon={item.icon} />
-									</button>
-								</li>
-							);
-						})}
+						<DndContext
+							sensors={sensors}
+							onDragEnd={handleDragEnd}
+							modifiers={[restrictToHorizontalAxis]}
+						>
+							<SortableContext
+								items={socialLinks.map(
+									(item) => `${item.icon}-${item.link}`
+								)}
+								strategy={horizontalListSortingStrategy}
+							>
+								{socialLinks.map((item, index) => {
+									return (
+										<SortableItem
+											key={`${item.icon}-${item.link}`}
+											id={`${item.icon}-${item.link}`}
+											index={index}
+											selectedLink={selectedLink}
+											setSelectedLink={setSelectedLink}
+											icon={item.icon}
+										/>
+									);
+								})}
+							</SortableContext>
+						</DndContext>
 						{isSelected && (
 							<li className="wp-block-blocks-course-team-member-add-icon-li">
 								<Tooltip
